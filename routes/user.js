d28890975877e03173f220d91bed70a56ca3a9a9
johann <git@johann-hofmann.com>
2013-10-26 17:45:36 +0200

    add mongoose dep

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,16 @@
 
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  androidDeviceID : String,
+  iosDeviceID : String,
+  number : String
+});
+
+var User = mongoose.model('User', userSchema);
+
 /*
  * GET users listing.
  */
